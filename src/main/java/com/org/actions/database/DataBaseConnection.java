fa60cb16ffbfd8c724b9125b871252de7dca087d
ericbhatti <bhattieric@gmail.com>
2015-12-28 21:03:53 +0500

    DataBase Properties moved to file

--- src/main/java/com/org/actions/database/DataBaseConnection.java
+++ src/main/java/com/org/actions/database/DataBaseConnection.java
@@ -1,5 +1,13 @@
 package com.org.actions.database;
 
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.sql.*;
 
 /**
@@ -7,29 +15,72 @@ import java.sql.*;
  */
 public class DataBaseConnection {
 
+    private static final String PROPERTIES_FILE = "properties.txt";
+    private static final String JSON_KEY_HOST = "host";
+    private static final String JSON_KEY_USERNAME = "username";
+    private static final String JSON_KEY_DB_NAME = "database";
+    private static final String JSON_KEY_PASS = "password";
+
+    private Connection dbConnection;
+
+    public DataBaseConnection()
+    {
+        InputStream input = Thread.currentThread().getContextClassLoader().getResourceAsStream(PROPERTIES_FILE);
+        InputStreamReader is = new InputStreamReader(input);
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(is);
+        String read = null;
+        try {
+            read = br.readLine();
+            while (read != null) {
+                sb.append(read);
+                read = br.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String json = sb.toString();
+
+        String dbUrl = "jdbc:mysql://";
+        String username = null;
+        String password = null;
+        try {
+            JSONObject jsonObject = new JSONObject(json);
+            String host = jsonObject.getString(JSON_KEY_HOST);
+            String database = jsonObject.getString(JSON_KEY_DB_NAME);
+            username = jsonObject.getString(JSON_KEY_USERNAME);
+            password = jsonObject.getString(JSON_KEY_PASS);
+            dbUrl = dbUrl + host + "/" + database;
+            System.out.println(dbUrl);
+            System.out.println(username);
+            System.out.println(password);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        try {
+            dbConnection = DriverManager.getConnection(dbUrl,
+                    username, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getDbConnection() {
+        return dbConnection;
+    }
 
     public void test()
     {
-        String dbUrl = "jdbc:mysql://localhost/virtual_tour";
-        String dbClass = "com.mysql.jdbc.Driver";
         String query = "Select * from weekdays";
-        String username = "root";
-        String password = "root";
         try {
-
-            Class.forName(dbClass);
-            Connection connection = DriverManager.getConnection(dbUrl,
-                    username, password);
-            Statement statement = connection.createStatement();
+            Statement statement = dbConnection.createStatement();
             ResultSet resultSet = statement.executeQuery(query);
             while (resultSet.next()) {
                 String tableName = resultSet.getString(2);
                 System.out.println("Day name : " + tableName);
             }
-            connection.close();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
+        dbConnection.close();
+        }  catch (SQLException e) {
             e.printStackTrace();
         }
     }
